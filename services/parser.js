66d699ed4b7b31b7fab9e5210324d91fba000b23
Raphael Dynk Salvador <rsalvador1@luc.edu>
2019-02-19 20:58:55 +0100

    inserted apiKey

--- services/parser.js
+++ services/parser.js
@@ -1,10 +1,44 @@
+const isApiKeyValid = (apiKey) => {
+  if(
+    !apiKey
+    || typeof apiKey !== 'string'
+    || !apiKey.length) {
+    return false;
+  }
+  const apiKeyArray = apiKey.split('=');
+  if(apiKeyArray.length !== 2) {
+    return false;
+  }
+  if(apiKeyArray[0].toLowerCase() !== 'api_key'){
+    return false;
+  }
+  if(apiKeyArray[0].length === 0){
+    return false;
+  }
+  return true;
+};
+
 const cli = (input) => {
-  if(!input || typeof input !== 'string') {
+  if(
+    !input
+    || typeof input !== 'string'
+    || !input.length
+  ) {
     return {err: 'not valid input'};
   }
-  return {parsed: input};
+  const inputArray = input.split(' ');
+  const apiKey = inputArray[0];
+  if(!isApiKeyValid(apiKey)) {
+    return {err: 'not valid apiKey'};
+  }
+
+  const parsed = {
+    apiKey
+  };
+  return {parsed};
 };
 
 module.exports = {
-  cli
+  cli,
+  isApiKeyValid
 };
--- tests/unit/parser.js
+++ tests/unit/parser.js
@@ -2,11 +2,38 @@ const assert = require('assert');
 const parserService = require('../../services/parser');
 describe('Main functions', ()=> {
   it('Should not parse invalids input', () => {
-    const invalidInputs = ['', [], 2];
+    const invalidInputs = ['', [], 2, ''];
     for(const ip of invalidInputs) {
       const { parsed, err } = parserService.cli(ip);
       assert.notEqual(err, null);
       assert.equal(parsed, null);
     }
   });
+
+  it('Should reject wrong apikey input', () => {
+    const invalidInputs = [
+      'apikey=123',
+      '',
+      null,
+      'apikey=',
+      '=123'
+    ];
+    for(const ip of invalidInputs) {
+      assert.equal(false, parserService.isApiKeyValid(ip));
+    }
+  });
+
+  it('Should accept valid apikey input', () => {
+    const validInputs = [
+      'api_key=123',
+      'api_key=dasjk12',
+      'API_KEY=dasjk12',
+      'aPi_key=dasjk12',
+      'Api_keY=dasjk12',
+    ];
+    for(const ip of validInputs) {
+      assert.equal(true, parserService.isApiKeyValid(ip));
+    }
+  });
+
 });
